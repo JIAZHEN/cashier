31b7296d2f0b5858666f17cc60d2fa050a8f278c
Jiazhen Xie <fdf515@gmail.com>
2016-03-01 20:43:51 +0000

    Reorganise module

--- lib/item.rb
+++ lib/item.rb
@@ -1,11 +1,9 @@
-Module Cashier
-  class Item
-    def initialize(name:, unit:, price:, type:, barcode:)
-      @name = name
-      @unit = unit
-      @price = price
-      @type = type
-      @barcode = barcode
-    end
+class Item
+  def initialize(name:, unit:, price:, type:, barcode:)
+    @name = name
+    @unit = unit
+    @price = price
+    @type = type
+    @barcode = barcode
   end
 end
