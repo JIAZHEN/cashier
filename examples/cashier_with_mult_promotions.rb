1e11099e4bc8fa1ba79001f6f16ac93029f0a245
Joe Xie <fdf515@gmail.com>
2016-03-04 08:49:10 +0000

    Add more examples

--- examples/cashier_with_mult_promotions.rb
+++ examples/cashier_with_mult_promotions.rb
@@ -6,7 +6,13 @@ Cashier.items = {
   "ITEM000005" => Cashier::Item.new(name: "可口可乐", unit: "瓶", price: 3, type: "饮料", barcode: "ITEM000005")
 }
 
+# ITEM000001 has two promotions
 Cashier.promotions = [
+  Cashier::Promotion.new(barcode: "ITEM000001", qty: 1) do |info, promoted_items, promotion|
+    info[:total] = info[:price] * info[:qty] * 0.95
+    info[:saving] = info[:price] * info[:qty] * 0.05
+  end,
+
   Cashier::Promotion.new(barcode: "ITEM000001", qty: 3) do |info, promoted_items, promotion|
     promoted_items[promotion.barcode] = info[:qty] / promotion.qty
     info[:total] = info[:price] * (info[:qty] - info[:qty] / promotion.qty)
